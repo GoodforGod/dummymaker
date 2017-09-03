6265247b6c1d31c4a6d86fb0cad57f8b8398f5ec
GoodforGod <goodforgod.dev@gmail.com>
2017-09-03 19:41:00 +0300

    - Version 1.0.3 realise

--- src/main/java/io/dummymaker/export/BaseExporter.java
+++ src/main/java/io/dummymaker/export/BaseExporter.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  * @author GoodforGod
  * @since 31.05.2017
  */
-abstract class BaseExporter<T> extends BufferedFileWriter implements IExporter<T> {
+public abstract class BaseExporter<T> extends BufferedFileWriter implements IExporter<T> {
 
     private final Logger logger = Logger.getLogger(BaseExporter.class.getName());
 
