aee90e9c37acd6d405016e631905df86e16920f5
Anton Kurako <goodforgod.dev@gmail.com>
2019-10-18 00:02:38 +0300

    Unique annotation scanner test Resource scanner tests

--- src/test/java/io/dummymaker/scan/ResourceScannerTests.java
+++ src/test/java/io/dummymaker/scan/ResourceScannerTests.java
@@ -1,11 +1,29 @@
 package io.dummymaker.scan;
 
+import io.dummymaker.scan.impl.ResourceScanner;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collection;
+
 /**
- * ! NO DESCRIPTION !
+ * Resource scanner tests
  *
  * @author GoodforGod
  * @since 05.10.2019
  */
-public class ResourceScannerTests {
+public class ResourceScannerTests extends Assert {
 
+    @Test
+    public void validResourcesScanned() {
+        final ResourceScanner scanner = new ResourceScanner();
+        final Collection<String> packages = scanner.scan("io.dummymaker.writer");
+
+        assertNotNull(packages);
+        assertFalse(packages.isEmpty());
+
+        assertTrue(packages.contains("io.dummymaker.writer.impl"));
+        assertTrue(packages.contains("io.dummymaker.writer.IWriter.class"));
+        assertTrue(packages.contains("io.dummymaker.writer.impl.BufferedFileWriter.class"));
+    }
 }
