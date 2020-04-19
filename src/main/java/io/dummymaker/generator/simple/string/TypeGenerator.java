50da1c1530e8d9d5e6bb9009c0c9f35fd52dab2e
Anton Kurako <goodforgod.dev@gmail.com>
2020-04-19 14:15:35 +0300

    GenVersion generator annotation added

--- src/main/java/io/dummymaker/generator/simple/string/TypeGenerator.java
+++ src/main/java/io/dummymaker/generator/simple/string/TypeGenerator.java
@@ -19,7 +19,8 @@ public class TypeGenerator implements IGenerator<String> {
 
     private final Pattern pattern = Pattern.compile("type|adjective|level|lvl", CASE_INSENSITIVE);
 
-    private final List<String> types = Arrays.asList("terrible", "bad", "poor", "limited", "neutral", "average", "Decent", "fine", "superior");
+    private final List<String> types = Arrays.asList("terrible", "bad", "poor", "limited", "neutral", "average", "Decent", "fine",
+            "superior");
 
     @Override
     public String generate() {
