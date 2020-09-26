f56228c6e5b15babf6090a2fd36438ed9e214d1d
Anton Kurako <goodforgod.dev@gmail.com>
2020-09-26 14:12:25 +0300

    Minor code cleaning

--- src/main/java/io/dummymaker/scan/impl/ResourceScanner.java
+++ src/main/java/io/dummymaker/scan/impl/ResourceScanner.java
@@ -124,7 +124,7 @@ public class ResourceScanner implements IScanner<String, String> {
     private List<URL> getSystemResources(String packageOrPath) {
         try {
             final String path = PackageUtils.toRelativePath(packageOrPath);
-            final Enumeration<URL> resourceUrls = this.getClass().getClassLoader().getResources(path);
+            final Enumeration<URL> resourceUrls = getClass().getClassLoader().getResources(path);
             if (!resourceUrls.hasMoreElements())
                 return Collections.emptyList();
 
