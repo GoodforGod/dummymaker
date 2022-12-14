1657b385710ceedb5079760ce8a6fe67960a5582
Anton Kurako <goodforgod.dev@gmail.com>
2022-12-14 23:46:26 +0300

    [4.0.0-SNAPSHOT] GeneratorException added

---
+++ src/main/java/io/dummymaker/error/GeneratorException.java
@@ -0,0 +1,12 @@
+package io.dummymaker.error;
+
+/**
+ * @author Anton Kurako (GoodforGod)
+ * @since 14.12.2022
+ */
+public class GeneratorException extends GenException {
+
+    public GeneratorException(String message) {
+        super(message);
+    }
+}
